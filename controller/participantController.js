f2c1d0baf478062b8ced2a34215fe77980c02c60
renakaagusta <renakaagusta28@gmail.com>
2021-01-10 17:38:51 +0700

    change participant

--- controller/participantController.js
+++ controller/participantController.js
@@ -27,11 +27,7 @@ var ip = [
 
 // Handle index actions
 exports.index = function (req, res) {
-  if (!ip.includes(req.ip.replace("::ffff:", ""))) {
-    console.log(req.ip.replace("::ffff:", ""));
-
-    return res.status(500).send();
-  }
+  
   Participant.get(function (err, participants) {
     if (err) {
       return res.json({
