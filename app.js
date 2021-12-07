e6a2c2dda8ffab033e7c738be43c54ff0d48aff8
renakaagusta <renakaagusta28@gmail.com>
2021-12-07 23:38:09 +0700

    setting cors

--- app.js
+++ app.js
@@ -8,10 +8,5 @@ const cloudinary = require('cloudinary').v2
 
 var app = express();
 
-app.all('*', function(req, res, next) {
-    var origin = req.get('origin'); 
-    res.header('Access-Control-Allow-Origin', origin);
-    res.header("Access-Control-Allow-Headers", "X-Requested-With");
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-});
+app.use(cors());
+app.options('*', cors());
