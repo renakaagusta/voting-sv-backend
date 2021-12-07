f7340b491a106f48cd8c2100491cfb579e2e22d2
renakaagusta <renakaagusta28@gmail.com>
2021-12-07 23:14:24 +0700

    setting cors

--- app.js
+++ app.js
@@ -49,8 +49,6 @@ app.get('/', function(req, res) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
     res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-
-    
 });
 
 // Body parse setup to handle post request
