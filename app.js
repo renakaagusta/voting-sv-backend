9849a559297500afa295b940280f2e7ad8885623
renakaagusta <renakaagusta28@gmail.com>
2021-12-07 23:22:32 +0700

    setting cors

--- app.js
+++ app.js
@@ -46,9 +46,10 @@ app.use(cors());
 app.options('*', cors());
 app.get('/', function(req, res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
-    res.setHeader('Access-Control-Allow-Credentials', true); // If needed
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
 });
 
 // Body parse setup to handle post request
