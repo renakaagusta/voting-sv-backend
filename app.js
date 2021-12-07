1fa9038b7cb331f7a3aed4be9e69d324a8e4c2cb
renakaagusta <renakaagusta28@gmail.com>
2021-12-08 00:51:11 +0700

    init

--- app.js
+++ app.js
@@ -20,6 +20,7 @@ cloudinary.config({
     api_secret: 'ML2n7mcn6zaDIMWeuToN7Hs2-rY'
 });
 
+// Import body parse
 let bodyParser = require('body-parser');
 // Import mongoose
 let mongoose = require('mongoose');
