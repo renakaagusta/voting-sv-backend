425a70a74eee4f7f296c1957eb62eb088759c0c5
renakaagusta <renakaagusta28@gmail.com>
2021-12-07 23:47:58 +0700

    setting cors

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//cors
+
 app.use(cors());
 app.options('*', cors());
 
